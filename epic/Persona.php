0ba8eceb9c17b2700ae48358e3fb8f5c250fdfca
Cole Alexander <cbielefeld@cnm.edu>
2020-05-02 10:00:17 -0600

    (data-design) Assignment 1 & 2

---
+++ epic/Persona.php
@@ -0,0 +1,23 @@
+<h1>Persona:</h1>
+<img src="post_550-web-150619_engineering-9896-2.jpg">
+    <li>Lucy Sterling,
+    <li>College undergraduate,
+    <li>Age 22,
+        <li>Lives in LA, California
+    <li>White/Hispanic
+    <li>Studies engineering at UCLA
+    <li>Speaks spanish & english fluently
+<li>Relevant Technology: Custom PC her & her brother built together
+<li>Her brother is helping her navigate the site so she isn't encountering many frustrations
+    <li>Recently affected by the pandemic Lucy's friend group started getting into virtual board games and have now suggested she get it too and join them on game nights
+    <li>As a new user, she wants to purchase the card game UNO so she can play with all her acquaintances and friend
+<ul>
+        <li>Behaviors & attitudes:
+<ul>
+    <li>Usually fairly calm and collected
+    <li>Very focused
+        <li>Dislikes having her email getting barraged with steam notifications
+
+
+
+
---
+++ epic/Use Case & Interaction Flow.php
@@ -0,0 +1,36 @@
+<h1>Use Case & Interaction Flow:</h1>
+
+<title>Title: Adding UNO to her steam library.</title>
+
+<h2>Description: Lucy wants to make use of the steam service & play UNO with her friends and family.</h2>
+
+<h3></h3>
+
+
+<ul>
+   <li>Now that she's activated her account, installed the client and types 'UNO' into the search bar, she
+        hits ENTER;
+    <ul>
+    <li>Steam takes her to a store page within the site, displaying a long list of relevant results
+    </ul>
+   <li> Her brother Tyler tells her that she'll need to click on the particular result she's searching for.
+        Tyler said it would be the one
+       on the very top of the page. She clicks on this result.
+    <ul>
+    <li>Steam opens up the purchase page of that particular game, displaying trailers, user feedback & pricing
+    </ul>
+            <li>Once Ms. Sterling decides this is the product she's looking for, she clicks the "Add to Cart" button
+    <ul>
+                <li>Steam proceeds to take her to checkout section
+                </ul>
+    <li>After she enters her card information
+    and hits the "Confirm Purchase" selector. Vuala!
+
+    <ul>
+                <li>Steam displays the message "Congratulations! This game is now in your steam library and you can install it anytime
+                </ul>
+
+                <li>After successful use of this service, Lucy uses this site frequently and with ease. (Around 4 times per week).</li>
+
+    <h6>Precondition: Lucy has to have created an account, and entered current & correct information into the system</h6>
+    <h7>Postcondition: The conditions are met and she recieves her game.</h7>
