580eee8f2bf8626607a21f2d4bde459413da8176
Cole Alexander <cbielefeld@cnm.edu>
2020-05-05 22:31:33 -0600

    (data-design) Assignment 1, 2 & 3 brought in the ERD diagram

--- epic/Entity Relationship Diagram.php
+++ epic/Entity Relationship Diagram.php
@@ -8,31 +8,6 @@
     <title>Document</title>
 </head>
 <body>
-CREATE TABLE `Order` (
-    `Order #` <type>,
-    `Customer_ID` <type>,
-        `Customer_name` <type>,
-            `Download Key` <type>,
-                PRIMARY KEY (`Order #`),
-                KEY `Key` (`Customer_ID`, `Customer_name`, `Download Key`)
-                );
-
-                CREATE TABLE `Product` (
-    `Product_ID` <type>,
-                    `Quanity` <type>,
-                        `Product_Type` <type>,
-                            KEY `Key` (`Product_ID`, `Quanity`, `Product_Type`)
-                            );
-
-                            CREATE TABLE `Customer` (
-    `Customer ID` <type>,
-                                `First Name` <type>,
-                                    `Last Name` <type>,
-                                        `Street` <type>,
-                                            `City` <type>,
-                                                `Zip` <type>,
-                                                    KEY `Key` (`Customer ID`, `First Name`, `Last Name`, `Street`, `City`, `Zip`)
-                                                    );
-
+<img src="ERD%20img.PNG">
 </body>
 </html>
